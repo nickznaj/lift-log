7be49dcdb254d9caacd56346ff7eccb8fecbc123
Nick Znajkowski <nickznajkowski@gmail.com>
2020-10-10 17:52:37 -0400

    finish get workout lambda

--- sam/lambdas/workout/GET/app.py
+++ sam/lambdas/workout/GET/app.py
@@ -9,6 +9,12 @@ def handler(event, context, config=None):
 
     workout = fetch_workout_for_date(date)
 
+    if not workout:
+        return {
+            "statusCode": 404,
+            "body": "Workouts for dates {} not found".format(date)
+        }
+
     return {
         "statusCode": 200,
         "body": json.dumps(workout)
