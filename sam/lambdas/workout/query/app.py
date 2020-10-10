eeec729a474a978de88a86197f1ad70a0459a71c
Nick Znajkowski <nickznajkowski@gmail.com>
2020-10-10 19:12:50 -0400

    begin query endpoint

--- sam/lambdas/workout/query/app.py
+++ sam/lambdas/workout/query/app.py
@@ -1,9 +1,24 @@
 import json
-from liftlog import pymysql
-    
+
+from liftlog.sql_helpers import fetch_workout_for_date
+
 
 def handler(event, context, config=None): 
     # add_workout(test)
+    query = json.loads(event['body'])
+    print(query)
+
+    if query.get('date', False):
+        date = query.get('date')
+        result = fetch_workout_for_date(date)
+
+    if not result:
+        return {
+            "statusCode": 404,
+            "body": "No workout(s) mathching query {}".format(event['body'])
+        }
+
     return {
-        "statusCode": 200
+        "statusCode": 200,
+        "body": json.dumps(result)
     }
