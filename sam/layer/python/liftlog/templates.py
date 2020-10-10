504b047c0c811ffcd46f7fbfb60fb1194d8a6421
Nick Znajkowski <nickznajkowski@gmail.com>
2020-10-10 14:48:37 -0400

    add templates file

---
+++ sam/layer/python/liftlog/templates.py
@@ -0,0 +1,44 @@
+workout_template = {
+    "date": "2020-10-01",
+    "id": 12,
+    "workout_notes": "paragraphs of brilliant insight",
+    "workout_coach_notes": "please do better",
+    "sets": []
+        # {   
+        #     "id": 73,
+        #     "exercise": "barbell squat",
+        #     "reps": 12,
+        #     "weight": 300,
+        #     "set_notes": "these were hard",
+        #     "set_coach_notes": "i'm the fucking king, bro",
+        #     "rpe": 12,
+        #     "link": "https://photos.google.com/5678"
+        # },
+        # {   
+        #     "id": 73,
+        #     "exercise": "barbell squat",
+        #     "reps": 12,
+        #     "weight": 300,
+        #     "set_coach_notes": "great work good job",
+        #     "rpe": 12
+        # },
+        # {   
+        #     "id": 73,
+        #     "exercise": "barbell bench press",
+        #     "reps": 12,
+        #     "weight": 300,
+        #     "set_notes": "easy work",
+        #     "rpe": 12,
+        #     "link": "https://photos.google.com/1234"
+        # }
+    # ]
+}
+
+set_template =  {   
+    "exercise": "Barbell Squat",
+    "reps": 12,
+    "weight": 300,
+    "set_notes": "these felt good.",
+    "set_coach_notes": "great work good job",
+    "rpe": 12
+}
