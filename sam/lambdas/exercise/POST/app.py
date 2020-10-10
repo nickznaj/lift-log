b6a638e7d104fba811603b3e98aaa6cf6ab164e2
Nick Znajkowski <nickznajkowski@gmail.com>
2020-10-10 18:23:23 -0400

    finish add exercise lambda

--- sam/lambdas/exercise/POST/app.py
+++ sam/lambdas/exercise/POST/app.py
@@ -1,45 +1,19 @@
 import json
 import sys
-from liftlog import pymysql
-from liftlog import sql_helpers
-from liftlog import sql_queries
-
-sys.path.append('/opt')
+from liftlog.pymysql.err import IntegrityError
+from liftlog.sql_helpers import add_exercise
 
 def handler(event, context, config=None): 
-    # add_workout(test)
-    workout = event['body']
+    exercise = json.loads(event['body'])
 
-    sql = ADD_WORKOUT.format(
-        date = workout['date'], 
-        workout_notes = workout.get('workout_notes', None)
-    )
-    
-    sql = "START TRANSACTION;\n" + sql
-    
-    # add each set to the db with needed foreign keys
-    for idx, sset in enumerate(workout['sets']):
-        exercise_id = fetch_exercise(sset['exercise'])['id']
-        sset['exercise_id'] = exercise_id
-        
-        link_id = None
-        if sset.get('link'):
-            link_id = "@link_id" + str(idx)
-            link_sql = add_link(sset.get('link', None), return_sql = True)
-            link_sql = link_sql.replace('@link_id', link_id)
-            sql += link_sql + "\n"
-        
-        
-        set_sql = add_set(sset, return_sql=True)
-        set_sql = set_sql.replace('@link_id', link_id)
-        sql += set_sql + "\n" 
-    
-    sql += "\nCOMMIT;"
-    
-    # sql = "COMMIT;"
-    
-    write_sql(sql)
-
+    try:
+        add_exercise(exercise)
+    except IntegrityError as e:
+        print(e.args)
+        return {
+            "statusCode": 422,
+            "body": json.dumps(e.args[1])
+        }
 
     return {
         "statusCode": 200
