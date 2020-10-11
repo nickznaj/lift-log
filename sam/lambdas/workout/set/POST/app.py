ae16d45123ea4ac19451c474411ad36d64f186c9
Nick Znajkowski <nickznajkowski@gmail.com>
2020-10-11 15:10:14 -0400

    finish add set lambda

--- sam/lambdas/workout/set/POST/app.py
+++ sam/lambdas/workout/set/POST/app.py
@@ -1,11 +1,11 @@
 import json
 from liftlog import pymysql
-from liftlog import sql_helpers
-from liftlog import sql_queries
+from liftlog.sql_helpers import add_link, add_set, fetch_exercise
+from liftlog.error_wrapper import error_wrapper
     
 # adds a new set to an existing workout
+@error_wrapper
 def handler(event, context, config=None): 
-    # add_workout(test)
     sset = event['body']
     workout_id = event['pathParameters']['workout_id']
     exercise_id = fetch_exercise(sset['exercise'])['id']
@@ -13,12 +13,9 @@ def handler(event, context, config=None):
     sset['exercise_id'] = exercise_id
     sset['workout_id'] = workout_id
 
-    if sset['link']:
+    if sset.get('link', False):
         link_id = add_link(sset['link'])
         sset['link_id'] = link_id
 
-    add_set(sset)
-
-    return {
-        "statusCode": 200
-    }
+    result = add_set(sset, adding_one=True, return_sql=False)
+    return result
