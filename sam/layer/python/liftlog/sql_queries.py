5a8669ab308c94a2a0f8de903defdb0ebd22b2ed
Nick Znajkowski <nickznajkowski@gmail.com>
2020-10-11 14:47:06 -0400

    clean up sql queries

--- sam/layer/python/liftlog/sql_queries.py
+++ sam/layer/python/liftlog/sql_queries.py
@@ -1,5 +1,5 @@
 FETCH_WORKOUT = """
-            select 
+            SELECT 
                 workout.id,
                 workout.workout_notes,
                 workout.workout_coach_notes,
@@ -10,7 +10,7 @@ FETCH_WORKOUT = """
                 `set`.rpe,
                 exercise.name as exercise,
                 link.link
-            from workout
+            FROM workout
                 left join `set` on `workout`.`id` = `set`.`fk_set_workout`
                 left join exercise on `set`.fk_set_exercise  = exercise.id
                 left join link on `set`.fk_set_link = link.id
@@ -18,29 +18,31 @@ FETCH_WORKOUT = """
         """
         
 FETCH_SETS = """
-    select 
+    SELECT 
         workout.date, 
         exercise.name, 
         `set`.reps, 
         `set`.weight,
         `set`.rpe
-    from workout
+    FROM workout
         inner join `set` on workout.id = `set`.fk_set_workout
         inner join exercise on `set`.fk_set_exercise = exercise.id
-    where 
-        exercise.name = "{NAME}"
+    WHERE 
+        exercise.name = "{name}"
 """
 
 FETCH_EXERCISE = """
     SELECT *
     FROM `log_lift`.`exercise`
-    WHERE exercise.name = "{NAME}";
+    WHERE exercise.name = "{name}";
 """
 
 ADD_WORKOUT = """
     INSERT INTO `log_lift`.`workout`
-        (`date`, `workout_notes`)
-        VALUES ("{date}", "{workout_notes}");
+    SET
+        `date` = {date}
+        `workout_notes` = {workout_notes}
+    ;
     
     SELECT LAST_INSERT_ID() INTO @workout_id;
 """
@@ -48,77 +50,47 @@ ADD_WORKOUT = """
 
 ADD_EXERCISE = """
     INSERT INTO `log_lift`.`exercise`
-        (`name`, `body_part`)
-        VALUES ("{name}", "{body_part}");
+    SET
+        `name` = {name}
+        `body_part` = {body_part}
     
-    SELECT LAST_INSERT_ID() INTO @workout_id;
+    SELECT LAST_INSERT_ID() INTO @exercise_id;
 """
-    
+
 
 ADD_SET = """
-	INSERT INTO `log_lift`.`set`
-	    (`reps`,
-	    `weight`,
-	    `rpe`,
-	    `fk_set_exercise`,
-	    `fk_set_link`,
-	    `fk_set_workout`,
-	    `set_coach_notes`,
-	    `set_notes`)
-	VALUES(
-	    {reps},
-	    {weight},
-	    {rpe},
-	    {exercise_id},
-	    @link_id,
-	    @workout_id,
-	    "{coach_notes}",
-	    "{set_notes}");
+    INSERT INTO `log_lift`.`set`
+    SET
+        `reps` = {reps},
+        `weight` = {weight},
+        `rpe` = {rpe},
+        `fk_set_exercise` = {exercise_id},
+        `fk_set_link` = @link_id,
+        `fk_set_workout` = @workout_id,
+        `set_coach_notes` = "{set_coach_notes}",
+        `set_notes` = "{set_notes}";
 """
 
 UPDATE_SET = """
     UPDATE `log_lift`.`set`
     SET
-    `reps` = {reps} 
-    `weight` = {weight} 
-    `rpe` = {rpe} 
-    `fk_set_exercise` = {exercise_id} 
-    `fk_set_link` = {link_id} 
-    `fk_set_workout` = {workout_id} 
-    `set_coach_notes` = {coach_notes} 
-    `set_notes` = {set_notes} 
+        `reps` = {reps} 
+        `weight` = {weight} 
+        `rpe` = {rpe} 
+        `fk_set_exercise` = {exercise_id} 
+        `fk_set_link` = {link_id} 
+        `fk_set_workout` = {workout_id} 
+        `set_coach_notes` = {set_coach_notes} 
+        `set_notes` = {set_notes} 
     WHERE `id` = {id};
 """
 
 
 ADD_LINK = """
 	INSERT INTO `log_lift`.`link`
-	(`link`)
-	VALUES
-	("{link}");
-	
+    SET
+	    `link` = "{link}"
+	;
     SELECT LAST_INSERT_ID() INTO @link_id;
 """
-
-
-
-
-# INSERT INTO `log_lift`.`workout`
-# (`date`, `workout_notes`)
-# VALUES ('2019-10-01', 'super duper');
-
-# -- store workout id
-
-# -- for set in sets: 
-#     -- look up exercise id based on name
-# 	INSERT INTO `log_lift`.`link`
-# 	(`link`)
-# 	VALUES
-# 	("supa dupah link");
-#     -- store link id
-
-# 	INSERT INTO `log_lift`.`set`
-# 	(`reps`,`weight`,`rpe`,`fk_set_exercise`,`fk_set_link`,`fk_set_workout`)
-# 	VALUES
-# 	(5,315,6,exercise_id,link_id,workout_id);
 
