46e6fd463caffe30b2f32a6f95a021cef485088c
Nick Znajkowski <nickznajkowski@gmail.com>
2020-10-11 15:11:53 -0400

    add body loading, headers to responses

--- sam/layer/python/liftlog/error_wrapper.py
+++ sam/layer/python/liftlog/error_wrapper.py
@@ -2,22 +2,35 @@ from liftlog.pymysql.err import IntegrityError
 from liftlog.cors import access_control_headers
 from functools import wraps
 import traceback
+import json
 
 errors = (
     IntegrityError, 
     KeyError
 )
 
+# turns any body from a string to a dict
+# runs the lambda, returning a 200 response with headers on success
+# catches errors and returns an appropriate response
 def error_wrapper(fn):
     @wraps(fn)
     def wrapped(event, context):
+        body = event.get('body', None)
         try:
-            return fn(event, context)
+            if isinstance(body, str):
+                event['body'] = json.loads(body)
+            response = fn(event, context)
+            return {
+                "statusCode": 200,
+                "body": json.dumps(response),
+                "headers": access_control_headers()
+            }
         except errors as e:
             print('ERROR: ', repr(e))
             traceback.print_exc(limit=5)
             return {
                 "statusCode": 422,
-                "body": repr(e)
+                "body": repr(e),
+                "headers": access_control_headers()
             }
     return wrapped
