eb0c762e6e51af93f508f0a7372da93f7dc57103
Nick Znajkowski <nickznajkowski@gmail.com>
2020-10-11 21:08:42 -0400

    add range query

--- sam/lambdas/workout/query/app.py
+++ sam/lambdas/workout/query/app.py
@@ -1,24 +1,28 @@
 import json
 
-from liftlog.sql_helpers import fetch_workout_for_date
+from liftlog.sql_helpers import fetch_workout_for_date, fetch_workouts_for_date_range
+from liftlog.error_wrapper import error_wrapper
 
 
+@error_wrapper
 def handler(event, context, config=None): 
-    # add_workout(test)
-    query = json.loads(event['body'])
-    print(query)
+    query = event['body']
 
-    if query.get('date', False):
+    if 'date' in query:
         date = query.get('date')
         result = fetch_workout_for_date(date)
+    elif set(['start', 'end']).issubset(query.keys()):
+        start = query.get('start')
+        end = query.get('end')
+        result = fetch_workouts_for_date_range(start, end)
 
+        
+
+# TODO: remove this by implementing custom error class
     if not result:
         return {
             "statusCode": 404,
             "body": "No workout(s) mathching query {}".format(event['body'])
         }
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps(result)
-    }
+    return result
